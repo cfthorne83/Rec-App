e8d3bd542d442acc5010e974365f2d281016cd38
cfthorne <cfthorne83@gmail.com>
2022-01-18 18:57:56 -0500

    finish routes

--- backend-express/routes/questions.js
+++ backend-express/routes/questions.js
@@ -1,24 +1,29 @@
 import express from "express";
+import { Question } from "../models/question.js";
 
 const router = express.Router();
 
 router.get( '/', (req, res) => {
-    const questions = [
-        { _id: 1, name: 'Vladimir Harkonnen', content: 'Am I the drama?' },
-        { _id: 2, name: 'Lady Jessica', content: 'Is Paul the Kwisatz Haderach?' },
-        { _id: 3, name: 'Paul Atreides', content: 'Why are my dreams so sandy?' },  
-    ]
-    res.json(questions);
+    Question.find()
+        .then(questions => res.json(questions))
+            .catch(err => res.status(404).json(err))
 })
 
 router.post( '/', (req, res) => {
-    const question = req.body;
-    res.json(question);
+    const newQuestion = new Question({
+        name: req.name,
+        content: req.content
+    });
+
+    newQuestion.save()
+        .then(question => res.json(question))
+            .catch(err => res.status(404).json(err))
 })
 
 router.delete('/:question_id', (req, res) => {
-    const _id = req.params.question_id;
-    res.json(_id);
+    Question.findOneAndDelete({ _id: req.params.question_id})
+        .then(question => res.json({ _id: question._id}))
+            .catch(err => res.status(404).json(err))
 })
 
 export const questions = router;
