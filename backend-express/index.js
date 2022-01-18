b6cd78724ac79d2d15717966fc38e663e6a3382e
cfthorne <cfthorne83@gmail.com>
2022-01-18 17:50:34 -0500

    add first routes

--- backend-express/index.js
+++ backend-express/index.js
@@ -1,10 +1,15 @@
 import express from "express";
+import { questions } from "./routes/questions.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/goobs', (req, res) => res.send("Gooble Gobble, I'm a turkey"))
+app.get('/goobs', (req, res) => {
+    res.send("Gooble Gobble, I'm a turkey")
+});
+
+app.use('/api/questions', questions)
 
 const port = process.env.PORT || 5000;
-app.listen( port, () => console.log("Server is alive!!"));
+app.listen( port, () => console.log(`Server is running on port ${port}!!`));
