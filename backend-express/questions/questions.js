941e5473f794a82ed4de4ce9b3d54054f1e2368f
cfthorne <cfthorne83@gmail.com>
2022-01-19 17:23:09 -0500

    modify routes

--- backend-express/questions/questions.js
+++ backend-express/questions/questions.js
@@ -1,25 +1,30 @@
 import express from "express";
+import { Question } from "../models/question.js";
 
 const QuestionsRouter = express.Router();
 
 QuestionsRouter.get('/', (req, res) => {
-    const questions = [
-            { _id: 1, name: 'Vladimir Harkonnen', content: 'Am I the drama?' },
-            { _id: 2, name: 'Lady Jessica', content: 'Is Paul the Kwisatz Haderach?' },
-            { _id: 3, name: 'Paul Atreides', content: 'Why are my dreams so sandy?' },
-        ];
-
-    res.json(questions);
+    Question.find()
+        .then(questions => res.json(questions))
+            .catch(err => res.status(404).json(err))
 })
 
 QuestionsRouter.post('/', (req, res) => {
-    res.json(req.body)
+    const newQuestion = new Question({
+        name: req.body.name, 
+        content: req.body.content
+    });
+
+    newQuestion.save()
+        .then(question => res.json(question))
+            .catch(err => res.status(404).json(err))
 })
 
 QuestionsRouter.delete('/:question_id', (req, res) => {
     const _id = req.params.question_id;
-
-    res.json(_id)
+    Question.findOneAndDelete({_id: _id})
+        .then(question => res.json({_id: question._id}))
+            .catch(err => res.status(404).json(err))
 })
 
 export default QuestionsRouter;
