3b8c0bce88d3db4cae232009a25d36997fb81fdf
cfthorne <cfthorne83@gmail.com>
2022-01-19 16:02:31 -0500

    connect to db

--- backend-express/index.js
+++ backend-express/index.js
@@ -1,9 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
 import { questions } from "./routes/questions.js";
+import { MONGO_URI } from "./config/keys.js";
 
 const app = express();
 
 app.use(express.json());
+
+mongoose
+    .connect( MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log("Connected to MongoDB!!"))
+            .catch(err => console.log(err));
 
 app.get('/goobs', (req, res) => {
     res.send("Gooble Gobble, I'm a turkey")
