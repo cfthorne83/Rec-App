11947f43822661d4c8eed03fd40d4a28ee7d6fce
cfthorne <cfthorne83@gmail.com>
2022-01-19 16:47:18 -0500

    resetup

--- backend-express/index.js
+++ backend-express/index.js
@@ -1,22 +1,9 @@
 import express from "express";
-import mongoose from "mongoose";
-import { questions } from "./routes/questions.js";
-import { MONGO_URI } from "./config/keys.js";
 
 const app = express();
 
 app.use(express.json());
 
-mongoose
-    .connect( MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => console.log("Connected to MongoDB!!"))
-            .catch(err => console.log(err));
+const port = process.env.port || 5000;
 
-app.get('/goobs', (req, res) => {
-    res.send("Gooble Gobble, I'm a turkey")
-});
-
-app.use('/api/questions', questions)
-
-const port = process.env.PORT || 5000;
-app.listen( port, () => console.log(`Server is running on port ${port}!!`));
+app.listen(port, () => console.log(`Server is running on ${port}`))
