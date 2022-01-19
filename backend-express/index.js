655efdfa238f56a90d6b02e9073f98dc7164ffba
cfthorne <cfthorne83@gmail.com>
2022-01-19 16:51:36 -0500

    test route

--- backend-express/index.js
+++ backend-express/index.js
@@ -6,4 +6,6 @@ app.use(express.json());
 
 const port = process.env.port || 5000;
 
+app.use('/goobs', (req, res) => res.send("Gooble, gobble. One of us!"))
+
 app.listen(port, () => console.log(`Server is running on ${port}`))
