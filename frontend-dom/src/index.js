8accec71dfc92375f1098e287645ab1cd1544e1b
cfthorne <cfthorne83@gmail.com>
2022-01-20 14:28:23 -0500

    set up skeleton

---
+++ frontend-dom/src/index.js
@@ -0,0 +1,7 @@
+const render = () => {
+
+    const root = document.getElementById('root');
+    const App = document.createElement('div');
+    root.append(App);
+}
+document.addEventListener('DOMContentLoaded', render());
