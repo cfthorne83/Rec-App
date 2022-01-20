ce5f3c7f1ad942c82ac5abad8074127bbe2f2480
cfthorne <cfthorne83@gmail.com>
2022-01-20 14:28:43 -0500

    save

--- frontend-dom/src/index.js
+++ frontend-dom/src/index.js
@@ -3,5 +3,7 @@ const render = () => {
     const root = document.getElementById('root');
     const App = document.createElement('div');
     root.append(App);
+
+    
 }
 document.addEventListener('DOMContentLoaded', render());
