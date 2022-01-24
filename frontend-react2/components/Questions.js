66c35332bce65b7ab8230a51f237247966384419
cfthorne <cfthorne83@gmail.com>
2022-01-24 16:09:11 -0500

    add questionsList

--- frontend-react2/components/Questions.js
+++ frontend-react2/components/Questions.js
@@ -2,29 +2,19 @@ import React from "react";
 
 export const Questions = ({questions}) => {
 
-    const generateQuestionsList = (questions) => {
-        questions.map(question => {
-            return(
-                <ul key={question._id}>
-                    <li>{question.name}</li>
-                    <li>{question.content}</li>
-                </ul>
-            )
-        })
-    };
-
+    const questionList = questions.map(question => {
+        return(
+            <ul key={question._id}>
+                <li>{question.name}</li>
+                <li>{question.content}</li>
+            </ul>
+        )   
+    });
+    
     return (
         <>
             <h1>Questions</h1>
-            {generateQuestionsList(questions)}
-            {/* {questions.map(question => {
-            return(
-                <ul key={question._id}>
-                    <li>{question.name}</li>
-                    <li>{question.content}</li>
-                </ul>
-            )
-        })} */}
+            {questionList}
         </>
     )
 };
