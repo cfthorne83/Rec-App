ba2e84fa731669255547c0e7335ad65d14673d7a
cfthorne <cfthorne83@gmail.com>
2022-01-31 16:00:38 -0500

    add store

---
+++ frontend-state/store/store.js
@@ -0,0 +1,13 @@
+import { createStore, configureStore, applyMiddleware, getDefaultMiddleware} from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import { QuestionsReducer } from "../reducers/questions_reducer";
+
+export const configureAppStore = () => {
+    const store = configureStore({
+        reducers: QuestionsReducer,
+        middleware: getDefaultMiddleware => getDefaultMiddleware()
+            .concat(process.env.NONE_ENV !== 'development' ? logger : []),
+        preloadedState: preloadedState
+    });
+    return store;
+}
